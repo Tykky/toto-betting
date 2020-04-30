898fb033def64659adb27ad411640f0780db972a
Tykky <40919314+Tykky@users.noreply.github.com>
2020-04-30 19:32:02 +0300

    Add confirm page to "place bets"

--- application/bets/views.py
+++ application/bets/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, abort
 from application import app, login_required, db
 from application.races.models import Race
 from application.bets.models import Bet
+from application.horses.models import Horse
 from application.bets.forms import PlaceBetForm
 from flask_login import current_user
 
@@ -11,26 +12,24 @@ def bets():
     races = Race.query.all()
     return render_template("/bets/bets.html",races = races, form = PlaceBetForm())
 
-@app.route("/bets/<raceid>", methods=['GET','POST'])
+@app.route("/bets/<raceid>", methods=['GET'])
 @login_required(role="USER")
 def place_bet(raceid):
     race = Race.query.get(raceid)
     if race and request.method == "GET":
         return render_template("/bets/place.html", race=race, form = PlaceBetForm())
 
-    if race and race.isopen:
-        form = PlaceBetForm(request.form)
-        amount = round(form.amount.data, 2)
-        if amount and current_user.credits - amount >= 0:
-            bet = Bet(amount, current_user.get_id(), raceid, 2)
-            current_user.credits = current_user.credits - amount
-            db.session().add(bet)
-            db.session().commit()
-        else:
-            return abort(400)
-    else:
-        return abort(400)
-
     return redirect(url_for('bets'))
 
+@app.route("/bets/<raceid>/confirm/<horseid>", methods=['GET','POST'])
+@login_required(role="USER")
+def place_bet_confirm(raceid,horseid):
+    if request.method == 'GET':
 
+        horse = Horse.query.get(horseid)
+        race = Race.query.get(raceid)
+
+        if horse and race:
+            return render_template("/bets/place_confirm.html",horse=horse, race=race)
+
+        return redirect(url_for('place_bet'))
